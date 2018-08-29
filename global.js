5fdc1cfec31b758d53c123c435855ccb086ecd31
rajeevb-shaadi <rajeev.b@peopleinteractive.in>
2018-08-29 12:47:06 +0530

    updated NW 1.0.6

---
+++ global.js
@@ -0,0 +1,17 @@
+var chromedriver = require('chromedriver');
+
+module.exports = {
+
+  before : function(done) {
+    chromedriver.start();
+
+    done();
+  },
+
+  after : function(done) {
+    chromedriver.stop();
+
+
+    done();
+  }
+};  
