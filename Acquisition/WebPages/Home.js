1aefc3f8d8fb17386505e6b0ce3c2b4bc44d0bf1
abcd <rajeev.b@peopleinteractive.in>
2018-12-23 11:48:13 +0530

    ii

--- Acquisition/WebPages/Home.js
+++ Acquisition/WebPages/Home.js
@@ -11,7 +11,7 @@ class Home
     
         this.browser
         .url("https://www.shaadi.com")
-        .waitForElementVisible("//div[@id='s2id_gender']")
+        .waitForElementVisible("div[id='s2id_gender']")
         .waitForElementVisible(registration_selector);
     }
 
