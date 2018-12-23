f794a7deaa4d405f40839aba51baa0512af9292d
abcd <rajeev.b@peopleinteractive.in>
2018-12-23 11:37:32 +0530

    chai

--- google.js
+++ google.js
@@ -2,9 +2,9 @@ module.exports = {
   'Demo test Google' : function (client) {
     client
       .url('http://www.google.com')
-      .waitForElementVisible('body', 9000)
+      .waitForElementVisible('//body', 9000)
       .assert.title('Google')
-      .assert.visible('input[type=text]')
+      .assert.visible('//input[@type=text]')
       .end();
   }
 };
